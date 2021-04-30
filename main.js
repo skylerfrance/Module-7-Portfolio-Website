d5fa8a0d0fa99f2f7293392a2cc3f4432bf7d3d6
Sky France <77553353+skylerfrance@users.noreply.github.com>
2021-04-30 13:18:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+/* Looping through images */
+
+for(let i = 1; i <= 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    displayedImage.src = e.target.src;
+  }
+}
